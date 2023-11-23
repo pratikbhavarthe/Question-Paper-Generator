eb7364166dc095eadea72b61783b224cf2f299db
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2023-11-23 13:37:51 +0530

    Question Paper Generator Commit

---
+++ questionPaperGenerator.js
@@ -0,0 +1,25 @@
+const questionStore = require('./questionStore');
+
+function filterQuestionsByDifficulty(difficulty) {
+    return questionStore.filter(question => question.difficulty === difficulty);
+}
+
+function getQuestionPaper(totalMarks, difficultyDistribution) {
+    const questionPaper = [];
+
+    for (const [difficulty, percentage] of Object.entries(difficultyDistribution)) {
+        const questions = filterQuestionsByDifficulty(difficulty);
+        const questionCount = Math.ceil((percentage / 100) * totalMarks);
+
+        console.log(`Difficulty: ${difficulty}, Questions: ${questions.length}, Required: ${questionCount}`);
+
+        questionPaper.push(...questions.slice(0, questionCount));
+    }
+
+    return questionPaper;
+}
+
+module.exports = {
+    filterQuestionsByDifficulty,
+    getQuestionPaper
+};

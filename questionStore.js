90883d72fb6a6df243271999384f2283705ed753
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2023-11-24 15:28:11 +0530

    Added few questions

--- questionStore.js
+++ questionStore.js
@@ -1,8 +1,13 @@
 const questionStore = [
     // I am adding some sample questions here
-    { question: "What is the speed of light", subject: "Physics", topic: "Waves", difficulty:"Easy", marks: 5},
+    { question: "What is the speed of light?", subject: "Physics", topic: "Waves", difficulty:"Easy", marks: 5},
     { question: "Is it possible to create magentic waves?", subject: "Physics", topic: "Waves", difficulty:"Easy", marks: 5 },
     { question: "Do atoms ever actually touch each other?", subject: "Physics", topic: "Atoms", difficulty:"Medium", marks: 10},
+    { question: "A gas is compressed isothermally. What is the change in entropy of the gas?", subject: "Physics", topic: "Thermodynamics", difficulty:"Hard", marks:20},
+    { question: "A laser beam is focused onto a small spot. What is the intensity of the light at the spot?", subject: "Physics", topic: "Optics", difficulty:"Hard", marks:20},
+    { question: "A photon is incident on a double slit. What is the pattern of the interference fringes?", subject: "Physics", topic: "Optics", difficulty:"Hard", marks:20},
+
+
 
 ]
 module.exports = questionStore;
